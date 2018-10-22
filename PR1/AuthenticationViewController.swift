6b331c165d2675acef29df0f75e7c2088e1c38d2
David Mendaño <david.mendano@movetia.com>
2018-10-22 23:18:04 +0200

    Ejercicio 4: Validación de la autenticación

--- PR1/AuthenticationViewController.swift
+++ PR1/AuthenticationViewController.swift
@@ -9,7 +9,26 @@ import UIKit
 
 class AuthenticationViewController: UIViewController, UITextFieldDelegate {
     // BEGIN-UOC-4
-
+    @IBOutlet var firstField: UITextField!
+    @IBOutlet var secondField: UITextField!
+    @IBOutlet var thirdField: UITextField!
+    @IBOutlet var fourthField: UITextField!
+    
+    @IBAction func editingTextField(_ sender: UITextField) {
+        if let text = sender.text, text.count > 1 {
+            sender.text = "\(text.dropLast(text.count - 1))"
+        }
+    }
+    
+    @IBAction func nextButtonTapped(_ sender: Any) {
+        // TODO: Se puede mejorar este código???
+        let code : String = "\(firstField?.text ?? "")\(secondField?.text ?? "")\(thirdField?.text ?? "")\(fourthField?.text ?? "")"
+        if Services.validate(code: code) {
+            performSegue(withIdentifier: "SegueToMainNavigation", sender: self)
+        } else {
+            Utils.show(Message: "Sorry, the entered code is not valid", WithTitle: "Error", InViewController: self)
+        }
+    }
     // END-UOC-4
     
     @IBAction func backButtonTapped(_ sender: UIButton) {
