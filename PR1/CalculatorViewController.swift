b92fa01ed75d4cd5495e8b44f5cbc9e2d6924289
David Mendaño <david.mendano@movetia.com>
2018-10-24 11:02:55 +0200

    Ejercicio 7: Añadidas constraints, IBOutlets y IBActions (falta formateo de decimales)

--- PR1/CalculatorViewController.swift
+++ PR1/CalculatorViewController.swift
@@ -9,16 +9,47 @@ import UIKit
 
 class CalculatorViewController: UIViewController {
     // BEGIN-UOC-7.1
-
+    @IBOutlet weak var initialAmountLabel: UILabel!    
+    @IBOutlet weak var initialAmountSlider: UISlider!
+    @IBOutlet weak var interestRateLabel: UILabel!
+    @IBOutlet weak var interestRateStepper: UIStepper!
+    @IBOutlet weak var yearsLabel: UILabel!
+    @IBOutlet weak var yearsStepper: UIStepper!
+    @IBOutlet weak var finalAmountLabel: UILabel!
     // END-UOC-7.1
     
     
     // BEGIN-UOC-7.2
-
+    @IBAction func fetchAndUpdateCurrentData(_ sender: Any) {
+        updateData(initialAmount: initialAmountSlider.value, interestRate: interestRateStepper.value, years: yearsStepper.value)
+    }
     // END-UOC-7.2
     
     
     // BEGIN-UOC-7.3
+    override func viewDidLoad() {
+        initializeData()
+    }
+    
+    func initializeData() {
+        initialAmountSlider.value = 100
+        interestRateStepper.value = 1
+        yearsStepper.value = 1
+        updateData(initialAmount: initialAmountSlider.value, interestRate: interestRateStepper.value, years: yearsStepper.value)
+    }
+    
+    func updateData(initialAmount : Float, interestRate : Double, years : Double) {
+        initialAmountLabel.text = "Initial amount: \(initialAmount) €"
+        interestRateLabel.text = "Interest rate: \(interestRate) %"
+        yearsLabel.text = "Years: \(years)"
 
+        finalAmountLabel.text = "Final amount: \(Services.calculateFinalAmount(ForAmount: initialAmount, WithInterest: interestRate, AndYears: years)) €"
+
+    }
+    
+    func getCurrentSliderValue() -> String{
+        let currentSliderValue = initialAmountSlider.value
+        return "\(currentSliderValue)"
+    }
     // END-UOC-7.3
 }
