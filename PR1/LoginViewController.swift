313edc368874bb8f65aeb0e59d75c94321491080
David Mendaño <david.mendano@movetia.com>
2018-10-24 08:54:33 +0200

    Ejercicio 6: Cambiado el fondo y el texto de bienvenida

--- PR1/LoginViewController.swift
+++ PR1/LoginViewController.swift
@@ -11,7 +11,7 @@ class LoginViewController: UIViewController {
     @IBOutlet weak var usernameField: UITextField!
     @IBOutlet weak var passwordField: UITextField!
     @IBOutlet weak var welcomeLabel: UILabel!
-
+    
     // BEGIN-UOC-2
     @IBAction func loginTapped(_ sender: UIButton) {
         var loginAllowed : Bool = false
@@ -35,7 +35,37 @@ class LoginViewController: UIViewController {
     // END-UOC-5
     
     // BEGIN-UOC-6
-
+    override func viewDidLoad() {
+        let hour : Int = getCurrentHour()
+        
+        if hour == -1 {
+            welcomeLabel.text = "Welcome"
+        } else if (hour >= 6) && (hour < 11) {
+            welcomeLabel.text = "Good morning"
+            self.view.backgroundColor = UIColor.orange.lighter()
+        } else if (hour >= 12) && (hour < 17) {
+            welcomeLabel.text = "Good afternoon"
+            self.view.backgroundColor = UIColor.yellow.lighter()
+        } else if (hour >= 18) && (hour < 22) {
+            welcomeLabel.text = "Good evening"
+            self.view.backgroundColor = UIColor.red.lighter()
+        } else {
+            welcomeLabel.text = "Good night"
+            self.view.backgroundColor = UIColor.blue.lighter()
+        }
+    }
+    
+    func getCurrentHour() -> Int {
+        let date = Date()
+        let calendar = Calendar.current
+        let component = calendar.dateComponents([.hour], from: date)
+        
+        if let hour = component.hour {
+            return hour
+        } else {
+            return -1
+        }
+    }
     // END-UOC-6
     
     @IBAction func unwindToLogin(for unwindSegue: UIStoryboardSegue, towardsViewController subsequentVC: UIViewController) {
